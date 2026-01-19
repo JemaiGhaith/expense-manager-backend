134f548c1619d46b74d137919883534e6cbb92b4
compte stagiaire <stagiaire@neopolis-dev.com>
2026-01-19 11:54:54 +0100

    first commit

---
+++ eurekaserver/src/test/java/com/coralio/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coralio.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
