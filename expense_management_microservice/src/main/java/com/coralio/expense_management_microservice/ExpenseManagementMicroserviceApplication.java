9c0cc7001e1c6441c3d41be6aded7b3429e7da6c
compte stagiaire <stagiaire@neopolis-dev.com>
2026-01-19 14:06:44 +0100

    second commit

---
+++ expense_management_microservice/src/main/java/com/coralio/expense_management_microservice/ExpenseManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.coralio.expense_management_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagementMicroserviceApplication.class, args);
+    }
+
+}
---
+++ expense_management_microservice/src/test/java/com/coralio/expense_management_microservice/ExpenseManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coralio.expense_management_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
