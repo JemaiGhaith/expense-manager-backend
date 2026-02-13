e47bf9ec15ea858ae652f6d4cfa19f23c214beab
compte stagiaire <stagiaire@neopolis-dev.com>
2026-02-13 17:23:47 +0100

    department integration

--- user_microservice/src/main/java/com/coralio/user_microservice/config/KeycloakUserDto.java
+++ user_microservice/src/main/java/com/coralio/user_microservice/config/KeycloakUserDto.java
@@ -1,6 +1,7 @@
 package com.coralio.user_microservice.config;
 
 import lombok.Data;
+import java.util.Map;
 
 @Data
 public class KeycloakUserDto {
@@ -10,16 +11,22 @@ public class KeycloakUserDto {
     private String email;
     private String firstName;
     private String lastName;
+    private String departmentId;      // ✅ AJOUTER
+    private String departmentName;    // ✅ AJOUTER (optionnel)
 
     public KeycloakUserDto(String id,
                            String username,
                            String email,
                            String firstName,
-                           String lastName) {
+                           String lastName,
+                           String departmentId,    // ✅ AJOUTER
+                           String departmentName) { // ✅ AJOUTER
         this.id = id;
         this.username = username;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.departmentId = departmentId;          // ✅ AJOUTER
+        this.departmentName = departmentName;      // ✅ AJOUTER
     }
 }
